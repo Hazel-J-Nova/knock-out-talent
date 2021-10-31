0190bd71bb9741758d8305e142ff0e7066a8cade
unknown <void.iros@gmail.com>
2021-10-31 14:39:50 -0500

    maybe

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname));
 app.use(express.json());
 app.use(
   mongoSanitize({
