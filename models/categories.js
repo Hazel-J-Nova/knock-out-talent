5a213c66fdee141a9e174f0cf45435748c83cfca
unknown <void.iros@gmail.com>
2021-10-31 13:02:09 -0500

    message

--- models/categories.js
+++ models/categories.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
-    title:{type:String,
-        required: true
+  title: { type: String, required: true },
+  content: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Content",
     },
-    content:  [{
-        type: Schema.Types.ObjectId,
-        ref: 'Content'}]
+  ],
 });
 
 module.exports = mongoose.model("Category", CategorySchema);
