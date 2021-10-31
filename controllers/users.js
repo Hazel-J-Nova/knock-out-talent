fa02a0f80cea83ace1f38220f3186019b288d2e7
unknown <void.iros@gmail.com>
2021-10-31 13:11:37 -0500

    a

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-const User = require("../models/users");
+const User = require("../models/Users");
 const { buildParams, sendConfirmationEmail } = require("../email/email");
 const fs = require("fs");
 const path = require("path");
